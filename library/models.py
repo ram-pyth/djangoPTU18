ed3e9a8ccb61968ea2ef440b2ca15e9cd74b9173
Ramunas P <ramunas.platukis@gmail.com>
2024-01-03 11:55:08 +0200

    paskaita 2 Book, Genre modeliai

--- library/models.py
+++ library/models.py
@@ -11,3 +11,22 @@ class Author(models.Model):
     def __str__(self):
         return f'{self.last_name} {self.first_name}'
 
+
+class Book(models.Model):
+    """Modelis reprezentuoja knygą-leidinį, ne konkretų
+    bibliotekos turimą fizinį egzempliorių"""
+    title = models.CharField('Pavadinimas', max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField('Aprašymas', max_length=1000, help_text='Trumpas knygos aprašymas')
+    isbn = models.CharField('ISBN', max_length=13)
+    genre = models.ManyToManyField('Genre', help_text='Išrinkite knygai žanrus')
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Genre(models.Model):
+    name = models.CharField('Pavadinimas', max_length=20, help_text='Įveskite žanro pavadinimą')
+
+    def __str__(self):
+        return f'{self.name}'
