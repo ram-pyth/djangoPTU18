b7b42384962a98969d0620f220ad2d2851f349bf
Ramunas P <ramunas.platukis@gmail.com>
2024-01-04 16:07:17 +0200

    paskaita 4 templates, orm naudojimas, suskaičiuojam eilutes lentelėse ir vaizudojam šablone rezultatus

--- library/views.py
+++ library/views.py
@@ -5,4 +5,16 @@ from .models import Author, Book, BookInstance, Genre
 
 
 def index(request):
-    return render(request, 'index.html')
+    # suskaičiuojam knygas ir jų egzempliorius
+    num_books = Book.objects.count()
+    num_bookinstances = BookInstance.objects.count()
+
+    # autorių skaičius
+    num_authors = Author.objects.count()
+
+    context_my = {
+        'num_books_t': num_books,
+        'num_bookinstances_t': num_bookinstances,
+        'num_authors_t': num_authors
+    }
+    return render(request, 'index.html', context=context_my)
